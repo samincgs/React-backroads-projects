0821a219e9775721829c504b0e960058cfa0bf2d
samin <samincgs@gmail.com>
2023-07-16 14:33:19 -0400

    created a data.js file that encapsulates all pageLinks and socialLinks in an array of objects for easier use

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
-import Services from './components/Services';
-import Tours from './components/Tours';
-import Footer from './components/Footer';
+import Services from './components/Services.js';
+import Tours from './components/Tours.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
